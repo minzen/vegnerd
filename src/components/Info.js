24e5b858a24a544d0eaa3fecccd5e07f5e4f6a7b
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2021-12-31 14:48:34 +0100

    Reorganizing the page layout

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,29 +1,53 @@
-import { Container, Grid } from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
+import React from 'react'
+import { Container, Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import Header from './Header'
+import Footer from './Footer'
 
 const useStyles = makeStyles({
-    infoBlock: {
+    body: {
+        paddingBottom: 10,
         paddingLeft: 10,
         paddingRight: 10,
-        backgroundColor: 'white'
+        fontSize: 14,
+        justifyContent: 'start'
+    },
+    header: {
+        paddingBottom: 15,
+        paddingTop: 15,
+        fontSize: 20
     }
 })
 
 const Info = () => {
-
     const classes = useStyles()
 
     return (
-        < Container maxWidth="xl" >
-            <Grid container direction="column">
-                <Grid item xs={12} className={classes.infoBlock}>
-                    Vegnerd is a Web app with the target of making some bookkeeping within gardening less painful. You can add and modify layouts and place garden plants there as well as information about their care in the app. Especially in the initial growing phase of vegetables and fruit it may be difficult to distinguish different plants from each other and it is this area that the app targets.  
+        <>
+            <Header />
+            <Container maxWidth="xl">
+                <Grid container direction="column">
+                    <Grid item xs={12}>
+                        <Typography variant="h3" className={classes.header}>
+                            About the app
+                        </Typography>
+                        <Typography variant="body1" className={classes.body}>
+                            Vegnerd is an app for hobby gardeners who want to manage information
+                            about garden plants and their specific characteristics by using a web browser.
+                            One may add notes specific to a plant, e.g. information about the
+                            fertilization, plant date and a general care instructions. It is    
+                            also planned that it is possible to create and manage layouts so
+                            you can have the same setup as in your own garden/greenhouse.
+                        </Typography>
+                        <Typography variant="body1" className={classes.body}>
+                            You will need a user account to be able to start using
+                            the service.
+                        </Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} className={classes.infoBlock}>
-                    You can create flexible layouts and place plants and set useful information on each plant to make the care an easier task.
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
+            <Footer />
+        </>
     )
 }
 export default Info
